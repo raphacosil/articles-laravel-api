0545e18442360ef2d2d31b7212733fcc25f19cb8
RaphaCosil <135926886+RaphaCosil@users.noreply.github.com>
2025-08-18 17:13:36 -0300

    Adding article repository

--- src/app/Repositories/ArticleRepository.php
+++ src/app/Repositories/ArticleRepository.php
@@ -2,7 +2,35 @@
 
 namespace App\Repositories;
 
+use app\Models\Article;
+
 class ArticleRepository
 {
+    public function getAll()
+    {
+        return Article::all();
+    }
 
+    public function getById($id)
+    {
+        return Article::findOrFail($id);
+    }
+
+    public function create(Article $article)
+    {
+        return Article::create($article);
+    }
+
+    public function update($id, Article $data)
+    {
+        $article = Article::findOrFail($id);
+        $article->update($data);
+        return $article;
+    }
+
+    public function delete($id)
+    {
+        $article = Article::findOrFail($id);
+        return $article->delete();
+    }
 }
