97c3fffbe57f0d710266968ef7b67180cef01fcb
RaphaCosil <135926886+RaphaCosil@users.noreply.github.com>
2025-08-18 18:03:41 -0300

    Adding article service and controller

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
-class ArticleController
+use App\Services\ArticleService;
+
+class ArticleController extends Controller
 {
+    protected $articleService;
+
+    public function __construct(ArticleService $articleService)
+    {
+        $this->articleService = $articleService;
+    }
 }
--- src/app/Services/ArticleService.php
+++ src/app/Services/ArticleService.php
@@ -2,7 +2,43 @@
 
 namespace App\Services;
 
+use App\Models\Article;
+use http\Exception\InvalidArgumentException;
+
 class ArticleService
 {
+    protected $articleRepository;
 
+    public function __construct(ArticleRepository $repository)
+    {
+        $this->articleRepository = $repository;
+    }
+
+    public function getAll()
+    {
+        return $this->articleRepository->getAll();
+    }
+
+    public function getById($id)
+    {
+        return $this->articleRepository->getById($id);
+    }
+
+    public function create(Article $article)
+    {
+        if (empty($article['title'] || empty($article['content']) || empty($article['sender_id']))) {
+            throw new \InvalidArgumentException("Missing content");
+        };
+        return $this->articleRepository->create($article);
+    }
+
+    public function update($id, Article $article)
+    {
+        return $this->articleRepository->update($id, $article);
+    }
+
+    public function delete($id)
+    {
+        return $this->articleRepository->delete($id);
+    }
 }
