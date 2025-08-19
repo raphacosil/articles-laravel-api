7df7fc18f48428ab32ed1d7d3e7800c7a9075e76
RaphaCosil <135926886+RaphaCosil@users.noreply.github.com>
2025-08-19 09:46:19 -0300

    Adding article controller configurations

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use App\Services\ArticleService;
 
 class ArticleController extends Controller
@@ -11,5 +12,33 @@ class ArticleController extends Controller
     public function __construct(ArticleService $articleService)
     {
         $this->articleService = $articleService;
+    }
+
+    public function index()
+    {
+        return response()->json($this->articleService->getAll());
+    }
+
+    public function show($id)
+    {
+        return response()->json($this->articleService->getById($id));
+    }
+
+    public function store(Request $request)
+    {
+        $article = $this->articleService->create($request->all());
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article = $this->articleService->update($id, $request->all());
+        return response()->json($article);
+    }
+
+    public function destroy($id)
+    {
+        $this->articleService->delete($id);
+        return response()->json(null, 204);
     }
 }
