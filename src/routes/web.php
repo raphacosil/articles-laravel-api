80f8670d53e26702704380ff5c95aee143ab35c7
RaphaCosil <135926886+RaphaCosil@users.noreply.github.com>
2025-08-19 18:24:45 -0300

    Database configuration, correcting compose and adding http routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('articles')->group(function () {
+    Route::get('', [ArticleController::class, 'index']);
+    Route::post('', [ArticleController::class, 'store']);
+    Route::get('/{id}', [ArticleController::class, 'show']);
+    Route::put('/{id}', [ArticleController::class, 'update']);
+    Route::delete('/{id}', [ArticleController::class, 'destroy']);
 });
